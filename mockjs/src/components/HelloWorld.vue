9c3ac7c009b84c354609a339054f63901966b636
時洋 <shiyang2016@sinosoft.com.cn>
2019-01-24 22:00:41 +0800

    commit mockjs

---
+++ mockjs/src/components/HelloWorld.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="hello">
+    <h2>淘宝书城</h2>
+    <ul>
+      <router-link tag="li" :to="item.href" v-for="(item, index) in messageList" :key="'list' + index">
+        <img :src="item.thumbnail_pic_s">
+        <div class="bookInfo">
+          <h4 v-text="item.title"></h4>
+          <div>单价：<strong>{{ item.price }}</strong>元/本</div>
+          <span>作者：{{ item.author_name }}</span>
+          <div>日期：{{ item.date }}</div>
+        </div>
+      </router-link>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      messageList: []
+    }
+  },
+  mounted () {
+    this.init()
+    this.timeout()
+  },
+  methods: {
+    async init () {
+      let res = await this.$axios.post('/news/index')
+      this.messageList = res.articles
+    },
+    timeout () {
+      let that = this
+      setTimeout(function () {
+        that.init()
+        that.timeout()
+      }, 3000)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+  width: 90%;
+  margin: 0 auto;
+  font-family: 'Microsoft YaHei';
+}
+li {
+  box-sizing: border-box;
+  width: 50%;
+  float: left;
+  padding: 10px;
+  text-align: left;
+  font-size: 14px;
+  color: #666;
+}
+li::after {
+  content: "";
+  display: table;
+}
+li img {
+  float: left;
+  margin-right: 20px;
+  width: 40%;
+  border-radius: 10px;
+}
+li h4 {
+  color: #333;
+  font-size: 16px;
+  font-weight: normal;
+  text-align: left;
+  margin-top: 0;
+  margin-bottom: 5px;
+}
+li .bookInfo {
+  float: left;
+  padding-left: 10px;
+}
+@media screen and (max-width: 400px) {
+li {
+  width: 50%;
+  font-size: 14px;
+}
+li img {
+  float: none;
+  width: 100%;
+}
+}
+</style>
---
+++ mockjs/src/mock/index.js
@@ -0,0 +1,27 @@
+// 引入mockjs
+const Mock = require('mockjs')
+// 获取 mock.Random 对象
+const Random = Mock.Random
+// mock一组数据
+const produceNewsData = function () {
+  let articles = []
+  for (let i = 0; i < 100; i++) {
+    let title = Random.csentence(5, 8)
+    let newArticleObject = {
+      title: title.substr(0, title.length - 2), //  Random.csentence( min, max )
+      thumbnail_pic_s: Random.dataImage('300x250', 'mock的图片'), // Random.dataImage( size, text ) 生成一段随机的 Base64 图片编码
+      price: Random.float(22, 399, 2, 2),
+      author_name: Random.cname(), // Random.cname() 随机生成一个常见的中文姓名
+      date: Random.date(), // Random.date()指示生成的日期字符串的格式,默认为yyyy-MM-dd；Random.time() 返回一个随机的时间字符串
+      href: '/' + Random.string('lower', 3, 6),
+      city: Random.city(true)
+    }
+    articles.push(newArticleObject)
+  }
+
+  return {
+    articles: articles
+  }
+}
+// Mock.mock( url, post/get , 返回的数据)；
+Mock.mock('/news/index', 'post', produceNewsData)
